3e7291b747b54d9621d0ea078140473754e66270
Sanil Kumar Ashwin <sanil.kumar.ashwin@gmail.com>
2021-07-29 21:32:45 +0100

    Added testing for log in method

--- lcsscaseapi/lcssclient.py
+++ lcsscaseapi/lcssclient.py
@@ -1,9 +1,20 @@
-import constants
+from lcsscaseapi import constants
 import requests
 
 class LCSSClient:
     def __init__(self, username, password):
         response = requests.post('https://' + constants.DOMAIN_NAME + constants.AUTH_ENDPOINT, 
                      data = {'username': username, 'password': password})
-        self._token = response.json()["token"]
+        if response.status_code == 200:
+            self._token = response.json()["token"]
+        elif response.status_code == 400:
+            if "Unable to log in with provided credentials" in str(response.content):
+                raise Exception("Incorrect log-in credentials")
+            elif "may not be blank" in str(response.content):
+                raise Exception("Incorrect log-in credentials: empty username or password")
+            else:
+                raise Exception("Unknown bad request, see response from server: " + str(response.content))
+        else:
+            raise Exception("Unknown error, see response from server: " + str(response.content))
+        
         
--- tests/test_lcssclient.py
+++ tests/test_lcssclient.py
@@ -1,5 +1,44 @@
-def test_always_passes():
-    assert True
+from .context import lcsscaseapi
+from lcsscaseapi.lcssclient import LCSSClient
+from lcsscaseapi import constants
+import pytest
 
-def test_always_fails():
-    assert False
+def test_init_successful(requests_mock):
+    requests_mock.post('https://' + constants.DOMAIN_NAME + constants.AUTH_ENDPOINT, json = {"token": "hi"})
+    client = LCSSClient(username="testing", password="123")
+    assert client._token == "hi"
+
+def test_init_wrong_login(requests_mock):
+    response_json = {
+        "non_field_errors": [
+            "Unable to log in with provided credentials."
+        ]
+    }
+    requests_mock.post('https://' + constants.DOMAIN_NAME + constants.AUTH_ENDPOINT, json = response_json, status_code = 400)
+    with pytest.raises(Exception, match="Incorrect log-in credentials"):
+        client = LCSSClient(username="testing", password="123")
+
+def test_missing_login(requests_mock):
+    response_json = {
+        "username": [
+            "This field may not be blank."
+        ],
+        "password": [
+            "This field may not be blank."
+        ]
+    }
+    requests_mock.post('https://' + constants.DOMAIN_NAME + constants.AUTH_ENDPOINT, json = response_json, status_code = 400)
+    with pytest.raises(Exception, match="Incorrect log-in credentials: empty username or password"):
+        client = LCSSClient(username="testing", password="123")
+
+def test_generic_badrequest_login(requests_mock):
+    response_json = {"whatever":"contents of error message"}
+    requests_mock.post('https://' + constants.DOMAIN_NAME + constants.AUTH_ENDPOINT, json = response_json, status_code = 400)
+    with pytest.raises(Exception, match="Unknown bad request.*contents of error message"):
+        client = LCSSClient(username="testing", password="123")
+
+def test_generic_error_login(requests_mock):
+    response_json = {"whatever":"contents of error message"}
+    requests_mock.post('https://' + constants.DOMAIN_NAME + constants.AUTH_ENDPOINT, json = response_json, status_code = 500)
+    with pytest.raises(Exception, match="Unknown error.*contents of error message"):
+        client = LCSSClient(username="testing", password="123")
