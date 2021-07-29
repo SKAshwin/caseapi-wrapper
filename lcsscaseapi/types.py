b6a324794eb25acda1644a6bb8fb048a965055af
Sanil Kumar Ashwin <sanil.kumar.ashwin@gmail.com>
2021-07-30 00:21:41 +0100

    Created CaseMeta type and renamed module

---
+++ lcsscaseapi/types.py
@@ -0,0 +1,10 @@
+class CaseMeta:
+    def __init__(self, case_id = "", case_name = "", title = "", doc_title = "", doc_id = "", doc_type = "", docket_number = "", outcome = ""):
+        self.case_id = case_id
+        self.case_name = case_name
+        self.title = title
+        self.doc_title = doc_title
+        self.doc_id = doc_id
+        self.doc_type = doc_type
+        self.docket_number = docket_number
+        self.outcome = outcome
